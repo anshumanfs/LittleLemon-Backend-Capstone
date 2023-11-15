69ccf1031a16d7f624f5707a7785b610b8d67474
Anshuman Nayak <64569737+anshuman61@users.noreply.github.com>
2023-11-15 11:21:11 +0530

    Creating Models and adding superuser

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "restaurant",
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,6 @@ DATABASES = {
         "PASSWORD": "root",
         "HOST": "127.0.0.1",
         "PORT": "3306",
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
